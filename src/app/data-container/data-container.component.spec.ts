1270c316d926aae119f8cabf2ea148a44cc757c7
RobertVerano <robertverano11895@gmail.com>
2024-09-23 15:54:42 +0800

    adding components

---
+++ src/app/data-container/data-container.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DataContainerComponent } from './data-container.component';
+
+describe('DataContainerComponent', () => {
+  let component: DataContainerComponent;
+  let fixture: ComponentFixture<DataContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DataContainerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DataContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
